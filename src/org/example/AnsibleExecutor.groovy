05b055925f8646bb4429f6e4d059facc0e7f2f55
Mohamed Tharik <94281905+tharik-109@users.noreply.github.com>
2025-02-05 16:09:37 +0530

    Update AnsibleExecutor.groovy

--- src/org/example/AnsibleExecutor.groovy
+++ src/org/example/AnsibleExecutor.groovy
@@ -4,10 +4,11 @@ class AnsibleExecutor {
     static void cloneRepo(script) {
         script.stage('Clone Repository') {
             script.echo "Cloning the repository..."
-            script.git url: 'https://github.com/your-repo.git', branch: 'main'
+            script.git url: 'https://github.com/tharik-109/jenkins-ansible-shared-library.git', branch: 'main'
         }
     }
 }
+
 static void runAnsible(script) {
     script.stage('Run Ansible Playbook') {
         script.sh '''
