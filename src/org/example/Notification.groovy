53825b867ebdb9e07725aff48997c30ee69f5902
Mohamed Tharik <94281905+tharik-109@users.noreply.github.com>
2025-02-05 16:06:26 +0530

    Update Notification.groovy

--- src/org/example/Notification.groovy
+++ src/org/example/Notification.groovy
@@ -6,7 +6,7 @@ class Notification {
             def slackChannel = script.readYaml(file: 'vars/config.yaml').SLACK_CHANNEL_NAME
             script.sh """
             curl -X POST --data-urlencode 'payload={"channel": "#${slackChannel}", "text": "${message}"}' \
-            https://hooks.slack.com/services/your/slack/webhook
+            https://hooks.slack.com/services/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
             """
         }
     }
